5c64a91ac077892e96eeddae0237d7af7d9c0913
s32965 <s32965@pjwstk.edu.pl>
2026-03-25 12:02:44 +0100

    feature-domains: Added User types and updated User class

---
+++ src/Enums/UserType.cs
@@ -0,0 +1,6 @@
+namespace APBD_Cw1_s32965.Enums;
+
+public enum UserType {
+  Student,
+  Employee
+}
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,9 +1,26 @@
+using APBD_Cw1_s32965.Enums;
+
 namespace APBD_Cw1_s32965.Models;
 
-public abstract class User(string firstName, string surname) {
+public abstract class User {
   private static int id = 0;
 
-  public int Id { get; } = id++;
-  public string FirstName { get; set; } = firstName;
-  public string Surname { get; set; } = surname;
+  public int Id { get; }
+  public string FirstName { get; set; }
+  public string Surname { get; set; }
+  public UserType Type { get; set; }
+  public int MaxRents { get; } = 0;
+
+  public User(string firstName, string surname, UserType type) {
+    Id = id++;
+    FirstName = firstName;
+    Surname = surname;
+    Type = type;
+    
+    if (type == UserType.Student) {
+      MaxRents = 2;
+    } else if (type == UserType.Employee) {
+      MaxRents = 5;
+    }
+  }
 }
