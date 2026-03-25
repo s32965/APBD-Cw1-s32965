3a342d68d565e7f575296251a66473d9113be26e
s32965 <s32965@pjwstk.edu.pl>
2026-03-25 11:48:50 +0100

    feature-domains: Added basic User class

---
+++ src/Models/User.cs
@@ -0,0 +1,9 @@
+namespace APBD_Cw1_s32965.Models;
+
+public abstract class User(string firstName, string surname) {
+  private static int id = 0;
+
+  public int Id { get; } = id++;
+  public string FirstName { get; set; } = firstName;
+  public string Surname { get; set; } = surname;
+}
